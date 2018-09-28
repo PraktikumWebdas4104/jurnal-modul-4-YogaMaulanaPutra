800ad5818bc2e35477aded7a69e957d528806232
YogaMaulanaPutra <43512249+YogaMaulanaPutra@users.noreply.github.com>
2018-09-28 10:06:57 +0700

    proses
    proses latihan

--- proses.php
+++ proses.php
@@ -1,4 +1,27 @@
 <?php
+		
+		if(isset($_POST['kirim'])) {
+			$username=$_POST=['username']
+			$password=$_POST=['password']
+			 $user=array(
+			 	"user"=>"yoga",
+			 	"pass"=>"maulana"):
 
+			 if ($username==$user['user']&&$password==$user['pass']){
+			 	echo "Berhasil SELALU:"."<br>>";
+
+			 	?>
+
+			 	<form method="POST">
+				 		NASI <input type="checkbox" name="latihan[]"" value="NASI"><BR><BR>
+				 		CILUNG <input type="checkbox" name="latihan[]"" value="CILUNG"><BR><BR>
+				 		SUSU <input type="checkbox" name="latihan[]"" value="SUSU"><BR><BR>
+				 		TAHU <input type="checkbox" name="latihan[]"" value="NASI"><BR><BR>
+				 		CHOOSE PHOTO
+
+			 	</form>
+
+
+			 	<input type="submit" name="kirim" value="input"> 
 
 ?>
